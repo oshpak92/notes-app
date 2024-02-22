4fa1e42ac6b30ae391a06636d4b09f597cdf3be8
Orest Shpak <orest@myrent.no>
2024-02-21 21:20:39 -0700

    UI template

---
+++ src/Notes.UI/notes-ui/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NotesListComponent } from './notes/notes-list/notes-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotesListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
